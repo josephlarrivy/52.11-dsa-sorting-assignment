f01223f840d24801aaa4abe9a99435d49310c75d
josephlarrivy <joseph.larrivy@gmail.com>
2023-06-15 16:12:21 -0500

    done with assignment

--- bubble.js
+++ bubble.js
@@ -2,12 +2,15 @@ function bubbleSort(arr) {
   let count = 0;
   for (let i = 0; i < arr.length; i++) {
     let swapped = false;
+    // console.log(swapped)
     for (let j = 0; j < arr.length - i; j++) {
       count++;
+      // console.log(count)
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        // console.log(arr[i], arr[j])
         swapped = true;
       }
     }
--- merge.js
+++ merge.js
@@ -10,12 +10,15 @@ function merge(arr1, arr2) {
       results.push(arr2[j]);
       j++;
     }
+    // console.log(arr1[i], arr2[j])
   }
   while (i < arr1.length) {
+    // console.log('pushing leftovers into results')
     results.push(arr1[i]);
     i++;
   }
   while (j < arr2.length) {
+    // console.log('pushing leftovers into results')
     results.push(arr2[j]);
     j++;
   }
@@ -24,11 +27,14 @@ function merge(arr1, arr2) {
 }
 
 function mergeSort(arr) {
-  //base case
-  if (arr.length <= 1) return arr;
+  if (arr.length <= 1) {
+    console.log('returning arr')
+    return arr;
+  }
   const mid = Math.floor(arr.length / 2);
   const left = mergeSort(arr.slice(0, mid));
   const right = mergeSort(arr.slice(mid));
+  // console.log(left, right)
   return merge(left, right);
 }
 
